37deb2932391b414ec23df35da28f57134173ae5
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-10-07 08:31:02 +0200

    Add vcc-ui elements to product component

--- components/product.tsx
+++ components/product.tsx
@@ -1,3 +1,30 @@
-export default function Product(){
-    return <div>I am a product</div>
+import { Card, CardContent, Flex, Spacer, Text } from "vcc-ui";
+import { ProductProps } from "../types";
+
+export default function Product(props: ProductProps) {
+  const { title, subtitle, type, imageUrl } = props;
+
+
+  console.log('props is', props)
+  return (
+    <Card>
+      <Flex
+        extend={{
+          justifyContent: "center",
+          padding: 8,
+        }}
+      >
+        <Text variant="bates" extend={{ color: "gray" }}>
+          {type}
+        </Text>
+      </Flex>
+      <CardContent>
+        <Text variant="ootah">{title}</Text>
+        <Spacer />
+        <Text>{subtitle}</Text>
+        <Spacer />
+        <img src={imageUrl}></img>
+      </CardContent>
+    </Card>
+  );
 }
