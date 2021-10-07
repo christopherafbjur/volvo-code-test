4aa85eeb54be3e4956cc2a104756a25efe1928b7
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-10-07 14:01:24 +0200

    Enhance Product component layout

--- components/product.tsx
+++ components/product.tsx
@@ -1,30 +1,49 @@
-import { Card, CardContent, Flex, Spacer, Text } from "vcc-ui";
+import Image from "next/image";
+import { Block, Card, Flex, Spacer, Text } from "vcc-ui";
+import { uppercase } from "../lib/helpers";
 import { ProductProps } from "../types";
+import ProductLink from "./productLink";
 
 export default function Product(props: ProductProps) {
-  const { title, subtitle, type, imageUrl } = props;
+  const { id, title, subtitle, type, imageUrl } = props;
 
-
-  console.log('props is', props)
   return (
     <Card>
+      <Block extend={{ padding: "10px" }}>
+        <Text subStyle="emphasis" variant="bates" extend={{ color: "gray" }}>
+          {uppercase(type)}
+        </Text>
+        <Flex
+          extend={{
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            flex: "0 0 auto",
+            width: "100%",
+          }}
+        >
+          <Text subStyle="emphasis">{title}</Text>
+          <Spacer />
+          <Text extend={{ color: "gray" }}>{subtitle}</Text>
+        </Flex>
+      </Block>
+      <Spacer />
+      {/* <img src={imageUrl}></img> */}
+      <Block extend={{ position: "relative", height: "200px" }}>
+        <Image layout="fill" src={imageUrl} alt={title} objectFit="cover" loading="lazy" />
+      </Block>
       <Flex
         extend={{
+          flexDirection: "row",
           justifyContent: "center",
-          padding: 8,
+          padding: "10px",
         }}
       >
-        <Text variant="bates" extend={{ color: "gray" }}>
-          {type}
-        </Text>
+        <ProductLink text="Learn" url={`/learn/${id}`} />
+        <Spacer />
+        <Spacer />
+        <Spacer />
+        <ProductLink text="Shop" url={`/shop/${id}`} />
       </Flex>
-      <CardContent>
-        <Text variant="ootah">{title}</Text>
-        <Spacer />
-        <Text>{subtitle}</Text>
-        <Spacer />
-        <img src={imageUrl}></img>
-      </CardContent>
     </Card>
   );
 }
