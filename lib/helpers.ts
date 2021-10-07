475a5e2c10650ed6706524153eb732b7a2f00d69
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-10-07 17:11:01 +0200

    Rename fn uppercase -> toUppercase, add getMinimumItemArray fn to solve carousel looping issue

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,3 +1,16 @@
-export function uppercase(text: string){
-    return text.toUpperCase()
+export function toUppercase(text: string) {
+  return text.toUpperCase();
+}
+
+export function getMinimumItemArray(inputItems: any, minimumAmount: number) {
+  var items = [...inputItems];
+
+  if (inputItems >= minimumAmount) return inputItems;
+  if (!inputItems.length) return inputItems;
+
+  while (items.length < minimumAmount) {
+    items = [...items, ...inputItems];
+  }
+
+  return items;
 }
