5aa3ef7b534183a71a8f79ea7abe752a3e62208b
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-10-07 17:14:49 +0200

    Refactor index page,  add DropDown component to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
-import type { GetStaticProps } from 'next'
-import Head from 'next/head'
-import Carousel from '../components/carousel'
-import { getAllCars } from '../lib/cars'
-import styles from '../styles/Home.module.css'
+import type { GetStaticProps } from "next";
+import Head from "next/head";
+import { useState } from "react";
+import Carousel from "../components/carousel";
+import DropDown from "../components/dropDown";
+import { getAllCars } from "../lib/cars";
+import styles from "../styles/Home.module.css";
 
 export const getStaticProps: GetStaticProps = () => {
   const allCarsData = getAllCars();
@@ -11,19 +13,36 @@ export const getStaticProps: GetStaticProps = () => {
       allCarsData,
     },
   };
-}
+};
 
 const Home = ({ allCarsData }) => {
+  const [cars, setCars] = useState(allCarsData);
+
+
+  const handleDropdownChange = async (selection) => {
+    if(selection === 'all') return setCars(allCarsData);
+    
+    const filtered = allCarsData.filter((car) => {
+      return car.bodyType.toLowerCase().includes(selection.toLowerCase());
+    });
+
+    setCars(filtered)
+  };
+
   return (
     <div>
       <Head>
         <title>Volvo Code Test</title>
-        <meta name="description" content="Volvo code assignment by Christopher Af Bjur" />
+        <meta
+          name="description"
+          content="Volvo code assignment by Christopher Af Bjur"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Carousel products={allCarsData}/>
+        <DropDown onChange={handleDropdownChange} />
+        <Carousel products={cars} />
       </main>
 
       <footer className={styles.footer}>
@@ -36,7 +55,7 @@ const Home = ({ allCarsData }) => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
