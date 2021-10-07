548666ab42679b64545f1469d269406602292776
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-10-07 14:07:34 +0200

    Add wrapper element to Arrow component of Chevron component

--- components/chevron.tsx
+++ components/chevron.tsx
@@ -1,9 +1,14 @@
-import { Arrow, Click } from "vcc-ui";
+import { Arrow, Block, Click } from "vcc-ui";
 
 export default function Chevron(props) {
   return (
-    <Click extend={{ background: "ghostwhite" }} onClick={props.onClick}>
-      <Arrow direction={props.direction} color="seagreen" size={20} />
+    <Click
+      extend={{ borderRadius: "50%", border: "1px solid gray", padding: "5px" }}
+      onClick={props.onClick}
+    >
+      <Block as='div' extend={{ width: "15px", height: "15px" }}>
+        <Arrow direction={props.direction} color="gray" size={15} />
+      </Block>
     </Click>
   );
 }
