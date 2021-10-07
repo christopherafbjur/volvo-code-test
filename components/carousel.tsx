973f89b23c65e5ca52f0638a44d08359943c5f86
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-10-07 08:37:02 +0200

    Setup keen-slider for carousel component

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,5 +1,98 @@
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
+import React from "react";
+import Product from "./product";
 
-export default function Carousel(){
+export default function Carousel({ data }) {
+  const [currentSlide, setCurrentSlide] = React.useState(0);
+  const [sliderRef, slider] = useKeenSlider({
+    loop: true,
+    initial: 0,
+    slidesPerView: 3,
+    slideChanged(s) {
+      setCurrentSlide(s.details().relativeSlide);
+    },
+  });
+  console.log("data", data);
 
-    return <div>Carousel</div>
+  return (
+    <>
+      <div className="navigation-wrapper">
+        <div ref={sliderRef} className="keen-slider">
+          {data.map((obj) => (
+            <div key={obj["id"]} className="keen-slider__slide number-slide1">
+              <Product
+                type={obj.bodyType}
+                title={obj.modelName}
+                subtitle={obj.modelType}
+                imageUrl={obj.imageUrl}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      {slider && (
+        <div className="dots">
+          {[...Array(slider.details().size).keys()].map((idx) => {
+            return (
+              <button
+                key={idx}
+                onClick={() => {
+                  slider.moveToSlideRelative(idx);
+                }}
+                className={"dot" + (currentSlide === idx ? " active" : "")}
+              />
+            );
+          })}
+        </div>
+      )}
+      {slider && (
+        <div
+          style={{
+            position: "relative",
+            width: "100px",
+            height: "100px",
+            backgroundColor: "red",
+          }}
+        >
+          <ArrowPrev
+            onClick={(e) => e.stopPropagation() || slider.prev()}
+            disabled={currentSlide === 0}
+          />
+          <ArrowNext
+            onClick={(e) => e.stopPropagation() || slider.next()}
+            disabled={currentSlide === slider.details().size - 1}
+          />
+        </div>
+      )}
+    </>
+  );
+}
+
+function ArrowPrev(props) {
+  const disabeld = props.disabled ? " arrow--disabled" : "";
+  return (
+    <svg
+      onClick={props.onClick}
+      className={"arrow arrow--left" + disabeld}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+    </svg>
+  );
+}
+
+function ArrowNext(props) {
+  const disabeld = props.disabled ? " arrow--disabled" : "";
+  return (
+    <svg
+      onClick={props.onClick}
+      className={"arrow arrow--right" + disabeld}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+    >
+      <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+    </svg>
+  );
 }
