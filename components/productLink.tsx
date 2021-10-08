7550e20594b333032fb6e3eb19814ff8847dfce2
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-10-08 15:15:38 +0200

    Add types + resolve Next.js linting recommendation regarding passHref

--- components/productLink.tsx
+++ components/productLink.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import { Link as VolvoLink } from "vcc-ui";
+import { ProductLinkProps } from "../types";
 
-export default function ProductLink({ url, text }) {
+export default function ProductLink({ url, text }: ProductLinkProps) {
   return (
     <>
-      <Link href={url}>
+      <Link href={url} passHref>
         <VolvoLink arrow="right">{text}</VolvoLink>
       </Link>
     </>
