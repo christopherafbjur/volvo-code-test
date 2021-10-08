417ac4b161b397991bb6e3d7678134fcb3754605
Christopher Af Bjur <chrisdevcabc@gmail.com>
2021-10-08 15:19:35 +0200

    Add function description

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -2,7 +2,9 @@ export function toUppercase(text: string) {
   return text.toUpperCase();
 }
 
+
 export function getMinimumItemArray(inputItems: any, minimumAmount: number) {
+  //Utility function which helps resolve looping/layout issues with KeenSlider generating an array of inputItems with a minimum length
   var items = [...inputItems];
 
   if (inputItems >= minimumAmount) return inputItems;
